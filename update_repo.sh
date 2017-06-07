9f4796f82a88c18047450b698e7cdfdf1e8d69d0
Fabian Ranacher <fra@deltapeak.de>
2017-06-07 14:24:23 +0200

    Allgemeines Update

--- update_repo.sh
+++ update_repo.sh
@@ -1,5 +1,5 @@
 sudo cp /etc/i3status.conf ./etc/i3status.conf
-cp -R ~/.config/i3 .config/i3
+cp -R ~/.config/i3 .config/
 echo "Kopiervorgang komplett"
 git add .
 git commit -m "Allgemeines Update"
